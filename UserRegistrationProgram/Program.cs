290e73637e5d137c8f5a48fc995cad2b8b9cc85a
sakshijadhav12 <sakshijjadhav28@gmail.com>
2023-08-19 21:48:23 +0530

    modiying program .cs to check whether firstname is valid or not

--- UserRegistrationProgram/Program.cs
+++ UserRegistrationProgram/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace UserRegistrationProgram
+﻿using System;
+using System.Text.RegularExpressions;
+namespace UserRegistrationProgram
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("User Registration Program");
+            Console.WriteLine("Enter first Name:");
+            string userInput = Console.ReadLine();
+            if (ValidateInput(userInput)) 
+            {
+                Console.WriteLine("{0} is valid first name", userInput);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not  valid first name plz enter valid one", userInput);
+            }
+        
+
+        }
+        public  static bool ValidateInput(string userInput)
+        {
+            string pattern = "^[A-Z][a-zA-Z]{2,}$";
+            return Regex.IsMatch(userInput, pattern);
+
+
         }
     }
 }
