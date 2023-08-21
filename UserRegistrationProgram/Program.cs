8996a9b9866b19ba84df5b01bfda2655e2d96979
sakshijadhav12 <sakshijjadhav28@gmail.com>
2023-08-21 21:17:47 +0530

    modifying program.cs to check that password has uppercase and one digit

--- UserRegistrationProgram/Program.cs
+++ UserRegistrationProgram/Program.cs
@@ -7,7 +7,7 @@ namespace UserRegistrationProgram
         static void Main(string[] args)
         {
             Console.WriteLine("User Registration Program");
-            Console.WriteLine("Enter a passward:");
+            Console.WriteLine("Enter a passward with a one digit ,one uppercase and it should have atleast 8 characters:");
             string userInput = Console.ReadLine();
             if (ValidateInput(userInput)) 
             {
@@ -15,14 +15,14 @@ namespace UserRegistrationProgram
             }
             else
             {
-                Console.WriteLine("given passwordis number not  valid ,plz enter password of atleadt 8 chrachters and one UpperCase");
+                Console.WriteLine("given passwordis number not  valid ,plz enter valid one");
             }
         
 
         }
         public  static bool ValidateInput(string userInput)
         {
-            string password = "^(?=.*[A-Z]).{8,}$";
+            string password = "^(?=.*[A-Z])(?=.*[0-9]).{8,}$";
             return Regex.IsMatch(userInput, password);
 
 
